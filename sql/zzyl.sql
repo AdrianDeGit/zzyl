e7e28873491d0591fc8b302be73105eab6f32606
clone <2894524580@qq.com>
2025-07-09 22:36:09 +0800

    feat：使用若依框架快速开发护理模块

--- sql/zzyl.sql
+++ sql/zzyl.sql
@@ -88,3 +88,108 @@ CREATE TABLE "nursing_level"
   COLLATE = utf8mb4_0900_ai_ci
   ROW_FORMAT = DYNAMIC COMMENT ='护理等级表';
 
+-- 菜单 SQL
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('护理项目', '2000', '1', 'project', 'serve/project/index', 1, 0, 'C', '0', '0', 'serve:project:list', '#',
+        'admin', sysdate(), '', null, '护理项目菜单');
+
+-- 按钮父菜单ID
+SELECT @parentId := LAST_INSERT_ID();
+
+-- 按钮 SQL
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('护理项目查询', @parentId, '1', '#', '', 1, 0, 'F', '0', '0', 'serve:project:query', '#', 'admin', sysdate(),
+        '', null, '');
+
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('护理项目新增', @parentId, '2', '#', '', 1, 0, 'F', '0', '0', 'serve:project:add', '#', 'admin', sysdate(), '',
+        null, '');
+
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('护理项目修改', @parentId, '3', '#', '', 1, 0, 'F', '0', '0', 'serve:project:edit', '#', 'admin', sysdate(), '',
+        null, '');
+
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('护理项目删除', @parentId, '4', '#', '', 1, 0, 'F', '0', '0', 'serve:project:remove', '#', 'admin', sysdate(),
+        '', null, '');
+
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('护理项目导出', @parentId, '5', '#', '', 1, 0, 'F', '0', '0', 'serve:project:export', '#', 'admin', sysdate(),
+        '', null, '');
+
+-- 菜单 SQL
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('护理计划', '2000', '1', 'plan', 'serve/plan/index', 1, 0, 'C', '0', '0', 'serve:plan:list', '#', 'admin',
+        sysdate(), '', null, '护理计划菜单');
+
+-- 按钮父菜单ID
+SELECT @parentId := LAST_INSERT_ID();
+
+-- 按钮 SQL
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('护理计划查询', @parentId, '1', '#', '', 1, 0, 'F', '0', '0', 'serve:plan:query', '#', 'admin', sysdate(), '',
+        null, '');
+
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('护理计划新增', @parentId, '2', '#', '', 1, 0, 'F', '0', '0', 'serve:plan:add', '#', 'admin', sysdate(), '',
+        null, '');
+
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('护理计划修改', @parentId, '3', '#', '', 1, 0, 'F', '0', '0', 'serve:plan:edit', '#', 'admin', sysdate(), '',
+        null, '');
+
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('护理计划删除', @parentId, '4', '#', '', 1, 0, 'F', '0', '0', 'serve:plan:remove', '#', 'admin', sysdate(), '',
+        null, '');
+
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('护理计划导出', @parentId, '5', '#', '', 1, 0, 'F', '0', '0', 'serve:plan:export', '#', 'admin', sysdate(), '',
+        null, '');
+
+-- 菜单 SQL
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('护理等级', '2000', '1', 'level', 'serve/level/index', 1, 0, 'C', '0', '0', 'serve:level:list', '#', 'admin',
+        sysdate(), '', null, '护理等级菜单');
+
+-- 按钮父菜单ID
+SELECT @parentId := LAST_INSERT_ID();
+
+-- 按钮 SQL
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('护理等级查询', @parentId, '1', '#', '', 1, 0, 'F', '0', '0', 'serve:level:query', '#', 'admin', sysdate(), '',
+        null, '');
+
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('护理等级新增', @parentId, '2', '#', '', 1, 0, 'F', '0', '0', 'serve:level:add', '#', 'admin', sysdate(), '',
+        null, '');
+
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('护理等级修改', @parentId, '3', '#', '', 1, 0, 'F', '0', '0', 'serve:level:edit', '#', 'admin', sysdate(), '',
+        null, '');
+
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('护理等级删除', @parentId, '4', '#', '', 1, 0, 'F', '0', '0', 'serve:level:remove', '#', 'admin', sysdate(), '',
+        null, '');
+
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('护理等级导出', @parentId, '5', '#', '', 1, 0, 'F', '0', '0', 'serve:level:export', '#', 'admin', sysdate(), '',
+        null, '');
+
