00146be20fc2ce78f3e389b53daa1a0b4d0a6746
AdrianDeGit <adriandeemail@gmail.com>
2025-07-17 16:46:20 +0800

    feat: 使用Redis缓存护理等级列表数据

--- zzyl-nursing-platform/src/main/java/com/zzyl/serve/service/impl/NursingLevelServiceImpl.java
+++ zzyl-nursing-platform/src/main/java/com/zzyl/serve/service/impl/NursingLevelServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.zzyl.common.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import com.zzyl.serve.mapper.NursingLevelMapper;
 import com.zzyl.serve.domain.NursingLevel;
@@ -14,6 +15,7 @@ import com.zzyl.serve.mapper.NursingLevelMapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 护理等级Service业务层处理
@@ -25,6 +27,11 @@ import java.util.Arrays;
 public class NursingLevelServiceImpl extends ServiceImpl<NursingLevelMapper, NursingLevel> implements INursingLevelService {
     @Autowired
     private NursingLevelMapper nursingLevelMapper;
+
+    @Autowired
+    private RedisTemplate<Object, Object> redisTemplate;
+
+    private static final String CACHE_KEY_PREFIX = "nursingLevel:all";
 
     /**
      * 查询护理等级
@@ -57,7 +64,11 @@ public class NursingLevelServiceImpl extends ServiceImpl<NursingLevelMapper, Nur
     @Override
     public int insertNursingLevel(NursingLevel nursingLevel) {
         // nursingLevel.setCreateTime(DateUtils.getNowDate());
-        return save(nursingLevel) ? 1 : 0;
+        // return save(nursingLevel) ? 1 : 0;
+        int flag = nursingLevelMapper.insertNursingLevel(nursingLevel);
+        // 删除缓存
+        deleteCache();
+        return flag;
     }
 
     /**
@@ -69,7 +80,10 @@ public class NursingLevelServiceImpl extends ServiceImpl<NursingLevelMapper, Nur
     @Override
     public int updateNursingLevel(NursingLevel nursingLevel) {
         nursingLevel.setUpdateTime(DateUtils.getNowDate());
-        return updateById(nursingLevel) ? 1 : 0;
+        // return updateById(nursingLevel) ? 1 : 0;
+        int flag = nursingLevelMapper.updateNursingLevel(nursingLevel);
+        deleteCache();
+        return flag;
     }
 
     /**
@@ -80,7 +94,10 @@ public class NursingLevelServiceImpl extends ServiceImpl<NursingLevelMapper, Nur
      */
     @Override
     public int deleteNursingLevelByIds(Long[] ids) {
-        return nursingLevelMapper.deleteNursingLevelByIds(ids);
+        // return nursingLevelMapper.deleteNursingLevelByIds(ids);
+        int flag = nursingLevelMapper.deleteNursingLevelByIds(ids);
+        deleteCache();
+        return flag;
     }
 
     /**
@@ -91,7 +108,10 @@ public class NursingLevelServiceImpl extends ServiceImpl<NursingLevelMapper, Nur
      */
     @Override
     public int deleteNursingLevelById(Long id) {
-        return removeById(id) ? 1 : 0;
+        // return removeById(id) ? 1 : 0;
+        int flag = nursingLevelMapper.deleteNursingLevelById(id);
+        deleteCache();
+        return flag;
     }
 
     /**
@@ -101,9 +121,34 @@ public class NursingLevelServiceImpl extends ServiceImpl<NursingLevelMapper, Nur
      */
     @Override
     public List<NursingLevel> listAll() {
+        // LambdaQueryWrapper<NursingLevel> queryWrapper = new LambdaQueryWrapper<>();
+        // queryWrapper.eq(NursingLevel::getStatus, 1)
+        //         .orderByAsc(NursingLevel::getName);
+        // return list(queryWrapper);
+        // 从缓存中获取
+        List<NursingLevel> list = (List<NursingLevel>) redisTemplate.opsForValue().get(CACHE_KEY_PREFIX);
+        // 缓存中有数据，直接返回
+        if (list != null && !list.isEmpty()) {
+            return list;
+        }
+
+        // 从数据库查询
         LambdaQueryWrapper<NursingLevel> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(NursingLevel::getStatus, 1)
-                .orderByAsc(NursingLevel::getName);
-        return list(queryWrapper);
+        queryWrapper.eq(NursingLevel::getStatus, 1);
+        list = list(queryWrapper);
+
+        // 将数据写入缓存
+        redisTemplate.opsForValue().set(CACHE_KEY_PREFIX, list, 60 * 60 * 24 + (int) (Math.random() * 100), TimeUnit.SECONDS);
+
+        return list;
     }
+
+    /**
+     * 删除Redis缓存数据
+     */
+    private void deleteCache() {
+        // 删除缓存
+        redisTemplate.delete(CACHE_KEY_PREFIX);
+    }
+
 }
